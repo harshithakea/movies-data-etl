9d6632ed857f15cae72edc895b0dfb9ee6d3b67b
Harshitha <55593451+vHarshitha@users.noreply.github.com>
2026-02-05 17:38:11 +0530

    Add basic movies ETL pipeline using pandas

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def main():
+	print("Starting Movies ETL Pipeline")
+	data = {
+		"movie_id":[1,2,3],
+		"movie":['Hello', 'Hi Nanna', 'King'],
+		"quantity":[2,6,3],
+		"price":[120,150,200]
+	}
+	df = pd.DataFrame(data)
+	df["total_amount"]=df["quantity"]*df["price"]
+
+	print(df)
+	output_path = "data/processed/movies_processed.csv"
+	df.to_csv(output_path, index=False)
+
+	print(f"Processed data saved to {output_path}")
+if __name__ == "__main__":
+	main()
+
